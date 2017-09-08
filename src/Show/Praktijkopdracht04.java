2e124b10906731f67508716f5a65ba79ee8bca62
kevinmijwaart <kevin.mijwaart@student.rocvf.nl>
2017-09-08 10:36:50 +0200

    Praktijkopdracht van hoofdstuk 4

---
+++ src/Show/Praktijkopdracht04.java
@@ -0,0 +1,35 @@
+package Show;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Praktijkopdracht04 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(50 , 30 , 250 , 30);
+        g.drawString("Lijn" , 140 , 45);
+        g.drawRect(50 , 60 , 200 , 100);
+        g.drawString("Rechthoek" , 120 , 180);
+        g.drawRoundRect(50 , 200 , 200 , 100 , 40 , 40);
+        g.drawString("Afgeronde rechthoek" , 90 , 320);
+        g.setColor(Color.red);
+        g.fillRect(260 , 60 , 200 , 100);
+        g.setColor(Color.black);
+        g.drawOval(260 , 60 , 200 , 100);
+        g.drawString("Gevulde rechthoek met ovaal" , 270 , 180);
+        g.setColor(Color.red);
+        g.fillOval(260 , 200 , 200 , 100);
+        g.setColor(Color.black);
+        g.drawString("Gevulde ovaal" , 320 , 320);
+        g.setColor(Color.red);
+        g.fillArc(470 , 60 , 200 , 100 , 0 , 60);
+        g.setColor(Color.black);
+        g.drawOval(470 , 60 , 200 , 100);
+        g.drawString("Taartpunt met ovaal eromheen" , 480 , 180);
+        g.drawOval(535 , 225 , 70 , 70);
+        g.drawString("Cirkel" , 550 , 320);
+    }
+}
