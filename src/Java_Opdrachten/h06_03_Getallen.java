94ff674c94da03cf6cb9e5f59ba2cdab3269110e
kevinmijwaart <kevin.mijwaart@student.rocvf.nl>
2017-09-19 19:09:26 +0200

    6.3 af

---
+++ src/Java_Opdrachten/h06_03_Getallen.java
@@ -0,0 +1,25 @@
+package Java_Opdrachten;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class h06_03_Getallen extends Applet {
+
+    double a, b, c, uitkomst;
+
+
+    public void init() {
+        a = 8;
+        b = 8;
+        c = -2;
+        uitkomst = (a + b) / c;
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawString("Ik heb twee positieve getallen bij elkaar opgeteld", 20, 20);
+        g.drawString("en de uikomst gedeeld door een negatief getal en de ", 20, 35);
+        g.drawString("en de uitkomst daarvan is : " + uitkomst, 20, 50);
+    }
+}
