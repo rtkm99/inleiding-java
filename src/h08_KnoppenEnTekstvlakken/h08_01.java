6bb19f9d9b5d750e54666ebc5b06a7b8ac994307
kevinmijwaart <kevin.mijwaart@student.rocvf.nl>
2017-09-25 14:31:34 +0200

    8.1 af

---
+++ src/h08_KnoppenEnTekstvlakken/h08_01.java
@@ -0,0 +1,54 @@
+package h08_KnoppenEnTekstvlakken;
+
+import javax.swing.text.TextAction;
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class h08_01 extends Applet {
+    TextField tekstvak;
+    Label label;
+    String s;
+    Button knop;
+    Button knop2;
+
+
+    public void init() {
+        tekstvak = new TextField("Klik op mij");
+        knop = new Button("Ok");
+        knop.addActionListener(new KnopListener());
+        knop2 = new Button("Reset");
+        knop2.addActionListener(new KnopListener());
+        label = new Label("Typ iets in, in dit tekstvak");
+        tekstvak.addActionListener(new TekstvakListener());
+        add(label);
+        add(tekstvak);
+        add(knop);
+        add(knop2);
+        s = "";
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(s, 50, 50);
+    }
+
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            repaint();
+        }
+
+
+    }
+
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            tekstvak.setText(" ");
+            repaint();
+        }
+    }
+
+
+}
+
